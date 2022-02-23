ebfdec2250362a3bd79c60fc2324df45a4af1338
Amirhm95 <amirhm95@yahoo.com>
2022-02-23 21:48:22 +0330

    assets

--- assets/SpaceCraftNft_0.js
+++ assets/SpaceCraftNft_0.js
@@ -1,23 +1,11 @@
 {
-  tokenId: 0;
-name:	"SpaceCraftNft 0"
-description:	"The Fighters. After Funny Bones began to disappear, the Agency of the Meaty Bones formed as a defense against all threats. The Meaty Bones wear ancient armor and move in groups, patrolling Hungry Ghosts and other evil spirits attacking the Land of the Dead."
-image:	"https://tastybones.mypin…piX6bR31KmtRm1xd2KBYv6wm"
+tokenId: 0;
+name:	"SpaceCraftNft #0"
+description:	"Just Show Up ! We Are So Cool :D "
+image:	".\assets\SpaceCraftNft_1.png"
+"external_url": "https://github.com/amirhm95/SpaceCraftNft",
 attributes	
 0	
-trait_type:	"Background"
-value:	"GM"
-1	
-trait_type:	"Body"
-value:	"Vikings Armor"
-2	
-trait_type:	"Type"
-value:	"Meaty Bones"
-3	
-trait_type:	"Head"
-value:	"Gems"
-4	
-trait_type:	"Headwear"
-value:	"Green-Purple Beanie"
-
+trait_type:	"Eyes"
+value:	"Red"
 }
