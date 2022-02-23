48ceec4db0303d9faa3f7809e0c92c331e476b6f
Amirhm95 <amirhm95@yahoo.com>
2022-02-23 21:36:25 +0330

    assets

---
+++ assets/SpaceCraftNft_2.js
@@ -0,0 +1,10 @@
+{
+  tokenId: 2;
+name:	"SpaceCraftNft #2"
+description:	"Just Show Up ! We Are So Cool :D "
+image:	"assets\SpaceCraftNft_2.png"
+attributes	
+2	
+trait_type:	"Eyes"
+value:	"Orange"
+}
---
+++ assets/SpaceCraftNft_3 copy.js
@@ -0,0 +1,10 @@
+{
+  tokenId: 3;
+name:	"SpaceCraftNft #3"
+description:	"Just Show Up ! We Are So Cool :D "
+image:	"assets\SpaceCraftNft_2.png"
+attributes	
+3	
+trait_type:	"Eyes"
+value:	"Brown"
+}
